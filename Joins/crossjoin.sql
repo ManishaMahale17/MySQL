386ca88b93b36a267c1c00f6a5c26afe37c53fda
Manisha Mahale <100189110+ManishaMahale17@users.noreply.github.com>
2022-08-24 16:27:05 +0530

    Update crossjoin.sql

--- Joins/crossjoin.sql
+++ Joins/crossjoin.sql
@@ -4,3 +4,6 @@ The CROSS JOIN keyword returns all records from both tables (table1 and table2).
 SELECT column_name(s)
 FROM table1
 CROSS JOIN table2;
+
+
+The CROSS JOIN keyword returns all matching records from both tables whether the other table matches or not.
