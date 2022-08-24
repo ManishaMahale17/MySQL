40158f4b397835eba075baec132378df04689035
Manisha Mahale <100189110+ManishaMahale17@users.noreply.github.com>
2022-08-24 11:35:26 +0530

    Update leftrightjoin.sql

--- Joins/leftrightjoin.sql
+++ Joins/leftrightjoin.sql
@@ -1,1 +1,26 @@
-
+--------------------LEFT JOIN:--------------------------------------------------------
+Match data from table1 and table2 common data and also all data from table1 that is left join.
+LEFT JOIN returns all records from the left table (table1),and the matched records from the right table(table2).
+********SYNTHAX*************
+SELECT columns 
+FROM table1
+LEFT JOIN table2
+ON table1.column_name=table2.column_name;
+                |                  |
+              foriegn key         primary key
+****************************************************************************************
+SELECT *  
+FROM person
+LEFT JOIN city
+ON  person.city=city.cid;
+******************************************************************************************
+--------------------RIGHT JOIN--------------------------------------------------------
+The RIGHT JOIN keyword returns all records from the right table (table2), and the matching records (if any) from the left table (table1).
+SELECT column_name(s)
+FROM table1
+RIGHT JOIN table2
+ON table1.column_name = table2.column_name;
+SELECT *  
+FROM person
+RIGHT JOIN city
+ON  person.city=city.cid;
