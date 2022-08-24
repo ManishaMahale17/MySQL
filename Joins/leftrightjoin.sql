96f899ea6322b5fb19dffc73afa8455bfad44ff9
Manisha Mahale <100189110+ManishaMahale17@users.noreply.github.com>
2022-08-24 12:32:49 +0530

    Update leftrightjoin.sql

--- Joins/leftrightjoin.sql
+++ Joins/leftrightjoin.sql
@@ -13,6 +13,13 @@ SELECT *
 FROM person
 LEFT JOIN city
 ON  person.city=city.cid;
+---------------------------
+or
+SELECT p.id,p.name,p.age,p.gender,c.cityname 
+FROM person p LEFT JOIN city c
+ON person.city=city.cid
+WHERE gender="M";
+
 ******************************************************************************************
 --------------------RIGHT JOIN--------------------------------------------------------
 The RIGHT JOIN keyword returns all records from the right table (table2), and the matching records (if any) from the left table (table1).
@@ -20,7 +27,12 @@ SELECT column_name(s)
 FROM table1
 RIGHT JOIN table2
 ON table1.column_name = table2.column_name;
+-------------------------------------------------------------
 SELECT *  
 FROM person
 RIGHT JOIN city
 ON  person.city=city.cid;
+----------------------------------------------------------------
+SELECT p.id,p.name,p.age,p.gender,c.cityname
+FROM person p RIGHT JOIN city c
+ON person.city=city.cid;
